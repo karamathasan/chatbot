8f7c26bf0ee8e6b88560f602e3f842f5b9619883
Alvin Shin <ashin7kr@gmail.com>
2024-08-08 22:31:14 -0400

    background animation

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,16 +8,7 @@ import './globals.css'
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className='relative'>
-        <div className="absolute w-full bg-gray-50 min-h-screen flex items-center justify-center px-16 z-[1]">
-          <div className="relative w-full max-w-lg">
-            <div className="absolute top-0 -left-4 w-72 h-72 bg-purple-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob"></div>
-            <div className="absolute top-0 -right-4 w-72 h-72 bg-yellow-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-2000"></div>
-            <div className="absolute -bottom-8 left-20 w-72 h-72 bg-pink-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-4000"></div>
-          </div>
-        </div>
-        <div className="absolute float-left clear-left z-[2] w-full min-h-screen h-full bg-none">{children}</div>
-      </body>
+      <body>{children}</body>
     </html>
   )
 }
--- src/app/login/page.js
+++ src/app/login/page.js
@@ -2,7 +2,6 @@
 
 import { useState } from "react";
 
-
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 
@@ -13,6 +12,7 @@ import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 
 import { signInWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
 import { auth } from "../firebase";
+import Background from "../components/background";
 
 export default function Login() {
   /* useStates for login info */
@@ -44,7 +44,7 @@ export default function Login() {
   const login = async () => {
     try {
       const userCredential = await signInWithEmailAndPassword(auth, email, password);
-      const user = userCredential.user;
+      // const user = userCredential.user;
       notifyLoginSuccess()
       router.push("/")
     } catch (error) {
@@ -53,32 +53,35 @@ export default function Login() {
   }
 
   return (
-    <main className="w-screen h-screen flex items-center justify-center">
+    <main className="relative">
       <Toaster position="top-center" />
-      <motion.div
-        initial={{ y: -25, opacity: 0 }}
-        animate={{ y: 0, opacity: 1 }}
-        transition={{ ease: "easeInOut", delay: 0.25, duration: 0.75 }}
-        className="flex flex-col justify-center items-center w-full max-w-md shadow-md border p-4 rounded-[25px] space-y-[5rem] bg-[#f8f8ff] ">
-        <div className="w-full">
-          <h1 className="text-[45px] text[#333] text-left w-full">Welcome Back!</h1>
-          <p className="text[#333] text-left w-full">Enter Your Username & Password</p>
-        </div>
+      <Background />
+      <div className="w-screen h-screen min-h-screen flex items-center justify-center absolute float-left clear-left z-[2] bg-none">
+        <motion.div
+          initial={{ y: -25, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ ease: "easeInOut", delay: 0.25, duration: 0.75 }}
+          className="flex flex-col justify-center items-center w-full max-w-md shadow-md border p-4 rounded-[25px] space-y-[5rem] bg-[#f8f8ff] ">
+          <div className="w-full">
+            <h1 className="text-[45px] text[#333] text-left w-full">Welcome Back!</h1>
+            <p className="text[#333] text-left w-full">Enter Your Username & Password</p>
+          </div>
 
-        <div className="w-full flex flex-col justify-center items-center space-y-[1rem]">
-          <form className="flex flex-col space-y-[1rem] w-full justify-center select-none" onSubmit={(e) => e.preventDefault()}>
-            <input type="text" placeholder="Email" className="w-full border rounded-[25px] py-[0.5rem] px-[1rem]" value={email} onChange={(e) => setEmail(e.target.value)} />
-            <div className="flex">
-              <input type={(showPassword) ? "text" : "password"} placeholder="Password" className="w-full border rounded-[25px] py-[0.5rem] px-[1rem]" value={password} onChange={(e) => setPassword(e.target.value)} />
-              <span className={"flex justify-around items-center cursor-pointer"} onClick={togglePassword}>
-                <FontAwesomeIcon icon={(showPassword) ? faEye : faEyeSlash} className="absolute mr-10" />
-              </span>
-            </div>
-            <button type="submit" className="bg-gradient-to-r from-blue-500 to-blue-600 text-white rounded-[10px] p-2" onClick={login}>Sign In</button>
-          </form>
-          <Link href={"/signup"}>Or Create a New Account</Link>
-        </div>
-      </motion.div>
+          <div className="w-full flex flex-col justify-center items-center space-y-[1rem]">
+            <form className="flex flex-col space-y-[1rem] w-full justify-center select-none" onSubmit={(e) => e.preventDefault()}>
+              <input type="text" placeholder="Email" className="w-full border rounded-[25px] py-[0.5rem] px-[1rem]" value={email} onChange={(e) => setEmail(e.target.value)} />
+              <div className="flex">
+                <input type={(showPassword) ? "text" : "password"} placeholder="Password" className="w-full border rounded-[25px] py-[0.5rem] px-[1rem]" value={password} onChange={(e) => setPassword(e.target.value)} />
+                <span className={"flex justify-around items-center cursor-pointer"} onClick={togglePassword}>
+                  <FontAwesomeIcon icon={(showPassword) ? faEye : faEyeSlash} className="absolute mr-10" />
+                </span>
+              </div>
+              <button type="submit" className="bg-gradient-to-r from-blue-500 to-blue-600 text-white rounded-[10px] p-2" onClick={login}>Sign In</button>
+            </form>
+            <Link href={"/signup"}>Or Create a New Account</Link>
+          </div>
+        </motion.div>
+      </div>
     </main>
   );
 }
